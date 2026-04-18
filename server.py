c1f6d4871cecf5895f10e9232d10fad7ef5fa68d
Chocapikk <balgogan@protonmail.com>
2026-04-18 21:19:42 +0200

    Fix: Remove unused variable, fix formatting in server.py

--- server.py
+++ server.py
@@ -6,13 +6,18 @@ import os
 
 try:
     from config import PROGRAM_PATH, PROGRAM_CMD
-except Exception as e:
-    print("Make sure to uncomment the correct PROGRAM_PATH and PROGRAM_CMD in config.py")
-    
+except Exception:
+    print(
+        "Make sure to uncomment the correct PROGRAM_PATH and PROGRAM_CMD in config.py"
+    )
+
 
 async def broadcast_log(message, clients):
     for client in clients:
-        await client.send(json.dumps({"type": "foxdot_log", "data": message, "color": None}))
+        await client.send(
+            json.dumps({"type": "foxdot_log", "data": message, "color": None})
+        )
+
 
 # Handle WebSocket connections
 async def handle_websocket(websocket, path, foxdot_process, clients):
@@ -34,48 +39,50 @@ async def handle_websocket(websocket, path, foxdot_process, clients):
         clients.remove(websocket)
         print("Client disconnected")
 
+
 async def main():
     # Start FoxDot
-    try: 
+    try:
         foxdot_process = Popen(
             PROGRAM_CMD,
             cwd=PROGRAM_PATH,
             stdin=PIPE,
-            stdout=PIPE,  
-            stderr=PIPE, 
-            env={**os.environ, "PYTHONUNBUFFERED": "1"}
+            stdout=PIPE,
+            stderr=PIPE,
+            env={**os.environ, "PYTHONUNBUFFERED": "1"},
         )
 
         print(f"FoxDot started, pid: {foxdot_process.pid}")
     except Exception as e:
         print(f"Error starting FoxDot: {e}")
         print("##### IMPORTANT #####")
-        print("Make sure FoxDot or Renardo is correctly installed and the path is set in config.py")
-        
+        print(
+            "Make sure FoxDot or Renardo is correctly installed and the path is set in config.py"
+        )
+
         return
 
     # Set of connected clients
     clients = set()
-    
+
     # Function to handle FoxDot output
     async def handle_foxdot_output():
         buffer = []
         while True:
             line = await asyncio.get_event_loop().run_in_executor(
-                None, 
-                foxdot_process.stdout.readline
+                None, foxdot_process.stdout.readline
             )
             if line:
                 log_message = line.decode()
-                if '^' not in log_message or not log_message.replace('^', '').isspace():
+                if "^" not in log_message or not log_message.replace("^", "").isspace():
                     log_message = line.decode().strip()
                 print(log_message)
-                
+
                 buffer.append(log_message)
                 if not log_message or log_message.endswith((">>>", "...")):
                     if buffer:
                         await broadcast_log("\n".join(buffer), clients)
-                        buffer = []                
+                        buffer = []
 
     # start the task to handle FoxDot output in the background
     asyncio.create_task(handle_foxdot_output())
@@ -84,10 +91,11 @@ async def main():
     async with websockets.serve(
         lambda ws, path: handle_websocket(ws, path, foxdot_process, clients),
         "localhost",
-        1234
+        1234,
     ):
         print("WebSocket server started on port 1234")
         await asyncio.Future()  # run forever
+
 
 if __name__ == "__main__":
     asyncio.run(main())
